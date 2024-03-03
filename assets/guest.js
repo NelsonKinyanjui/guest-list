607652aafceb0577d26690125dcbce39839dfec9
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-03 12:33:48 +0300

    add div on html

---
+++ assets/guest.js
@@ -0,0 +1,8 @@
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.querrySelector('admitted');
+
+const refused = document.querrySelector('refused');
+
+admitted.textContent = 'Admit:';
+admitted.textContent = 'Refuse';
