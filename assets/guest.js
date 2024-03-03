911a225e775004ab0b43500965b0985569b6fa50
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-03 14:33:22 +0300

    debug code

--- assets/guest.js
+++ assets/guest.js
@@ -1,19 +1,19 @@
 const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
 
-const admitted = document.querrySelector('admitted');
+const admitted = document.querySelector('.admitted');
 
-const refused = document.querrySelector('refused');
+const refused = document.querySelector('.refused');
 
 admitted.textContent = 'Admit: ';
-admitted.textContent = 'Refuse: ';
+refused.textContent = 'Refuse: ';
 
-for(i=0; i < people.length; i++){
-    if (people === 'Phil' || people === 'Lola'){
-        refused.textContent +=', ';
+for(const person of people){
+    if (person === 'Phil' || person === 'Lola'){
+        refused.textContent +=`${person}, `;
 
     }
     else {
-        admitted.textContent += ', ';
+        admitted.textContent += `${person}, `;
     }
 
 }
