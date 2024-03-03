61ff5e343bf61e21842e9ad0a9edf89b4e6633a5
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-03 14:04:03 +0300

    add a for loop on pople array

--- assets/guest.js
+++ assets/guest.js
@@ -4,5 +4,16 @@ const admitted = document.querrySelector('admitted');
 
 const refused = document.querrySelector('refused');
 
-admitted.textContent = 'Admit:';
-admitted.textContent = 'Refuse';
+admitted.textContent = 'Admit: ';
+admitted.textContent = 'Refuse: ';
+
+for(i=0; i < people.length; i++){
+    if (people === 'Phil' || people === 'Lola'){
+        refused.textContent +=', ';
+
+    }
+    else {
+        admitted.textContent += ', ';
+    }
+
+}
